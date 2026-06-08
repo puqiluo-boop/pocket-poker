9846951fd729345b3d28f2a939669b57727a95b3
Puqi Luo <ptluo06@gmail.com>
2026-06-08 11:25:09 -0400

    Implemented blinds, checks, calls, and betting. TODO, folding and end of game handling (the latter will need cardToValue and handValue etc.)

--- src/Dealer/GameState/GameState.cpp
+++ src/Dealer/GameState/GameState.cpp
@@ -5,13 +5,126 @@
 
 #include <vector>
 #include <optional>
+#include <algorithm>
 
-GameState::GameState(const int deck[52], std::vector<String> playerMACS) : communityCards{deck[0], deck[1], deck[2], deck[3], deck[4]} {
-    for (size_t i = 0; i < playerMACS.size(); i++) {
-        PlayerState player = PlayerState(playerMACS[i], (uint32_t)1000, (uint8_t*)deck[i * 2]);
-        players.push_back(player);
+//Add parameter for button postion
+GameState::GameState(const int deck[52], const uint32_t smallBlind, const uint32_t bigBlind, const std::vector<String> playerMACS) :
+communityCards{deck[0], deck[1], deck[2], deck[3], deck[4]},
+smallBlind(smallBlind),
+bigBlind(bigBlind) {
+    PlayerState smallBlindPlayer = PlayerState(playerMACS[0], (uint8_t*)deck[5], (uint32_t)1000);
+    smallBlindPlayer.activeBet = std::min(smallBlind, smallBlindPlayer.remainingChips);
+    smallBlindPlayer.remainingChips -= std::min(smallBlind, smallBlindPlayer.remainingChips);
+    playersInHand.push_back(smallBlindPlayer);
+
+    PlayerState bigBlindPlayer = PlayerState(playerMACS[1], (uint8_t*)deck[7], (uint32_t)1000);
+    bigBlindPlayer.activeBet = std::min(bigBlind, bigBlindPlayer.remainingChips);
+    bigBlindPlayer.remainingChips -= std::min(bigBlind, bigBlindPlayer.remainingChips);
+    playersInHand.push_back(bigBlindPlayer);
+
+    for (size_t i = 2; i < playerMACS.size(); i++) {
+        PlayerState player = PlayerState(playerMACS[i], (uint8_t*)deck[5 + (i * 2)], (uint32_t)1000);
+        playersInHand.push_back(player);
     }
-    playerTurn = 0;
+
+    playerTurn = 2 % playersInHand.size(); // Start with player to the left of big blind
     street = 0;
+    callAmount = bigBlind;
+    lastAnyRaiserIndex = -1;
+    minRaiseIncrement = bigBlind;
+}
+
+/**
+ * Increments playerTurn to the next player. Should be called after a player takes a non-fold action.
+ */
+void GameState::incTurn(bool folding = false) {
+    if(lastAction()) {
+        nextStreet();
+    } else {
+        playerTurn = (playerTurn + folding ? 1 : 0) % playersInHand.size();
+        if(playersInHand[playerTurn].remainingChips == 0) { // Skip players who are all in
+            incTurn();
+        }
+    }
+}
+
+/**
+ * @return true if the current player's action will end the round
+ */
+bool GameState::lastAction() {
+    return (lastAnyRaiserIndex == -1 && playerTurn == playersInHand.size() - 1) || (playerTurn + 1) % playersInHand.size() == lastAnyRaiserIndex;
+}
+
+void GameState::nextStreet() {
+    street++;
+    playerTurn = 0;
+    callAmount = 0;
+    lastAnyRaiserIndex = -1;
+    minRaiseIncrement = bigBlind;
+    for(PlayerState& player : playersInHand) {
+        player.hasActed = false;
+        player.inactiveBet += player.activeBet;
+        player.activeBet = 0;
+    }
+    if(street == 4) {
+        // TODO: If game is over
+    }
+}
+
+bool GameState::check() {
+    if(callAmount == 0) {
+        playersInHand[playerTurn].hasActed = true;
+        incTurn();
+        return true;
+    }
+    return false;
+}
+
+bool GameState::call() {
+    if(callAmount == 0) {
+        return false;
+    }
+    PlayerState& player = playersInHand[playerTurn];
+    if(player.remainingChips < callAmount - player.activeBet) { // All-in call
+        player.activeBet += player.remainingChips;
+        player.remainingChips = 0;
+    } else {
+        player.remainingChips -= callAmount - player.activeBet;
+        player.activeBet += callAmount - player.activeBet;
+    }
+    player.hasActed = true;
+    incTurn();
+    return true;
+}
+
+bool GameState::betRaise(uint32_t amount) {
+    PlayerState& player = playersInHand[playerTurn];
+    if(callAmount - player.activeBet < minRaiseIncrement && player.hasActed) { // Player is not allowed to raise (can only call or fold) due to a short all-in
+        return false;
+    }
+    if(amount == player.remainingChips) { // All in
+        if(amount + player.activeBet <= callAmount) { // Constitutes as a call, not a raise
+            return false;
+        }
+        player.activeBet += player.remainingChips;
+        player.remainingChips = 0;
+    } else if(amount + player.activeBet - callAmount >= minRaiseIncrement && amount <= player.remainingChips) { // "Normal" bet or raise
+        player.remainingChips -= amount;
+        player.activeBet += amount;
+    } else {
+        return false;
+    }
+    // player.activeBet should now be the total amount the player has bet for this street.
+    if(player.activeBet - callAmount > minRaiseIncrement) {
+        minRaiseIncrement = player.activeBet - callAmount;
+    }
+    assert(player.activeBet > callAmount);
+    assert(minRaiseIncrement >= bigBlind);
+    callAmount = player.activeBet;
+    lastAnyRaiserIndex = playerTurn;
+
+    player.hasActed = true;
+    incTurn();
+    return true;
 }
 
--- src/Dealer/GameState/GameState.h
+++ src/Dealer/GameState/GameState.h
@@ -5,13 +5,15 @@
 
 struct PlayerState {
     const String playerMAC;
-    const uint32_t totalChips;
     const uint8_t holeCards[2];
 
-    uint32_t totalBet;
-    bool inHand;
+    uint32_t remainingChips;
+    uint32_t activeBet; // Amount currently in the pot for the current street
+    uint32_t inactiveBet; // Amount in the pot from previous streets.
 
-    PlayerState(String mac, uint32_t chips, uint8_t cards[2]) : playerMAC(mac), totalChips(chips), holeCards{cards[0], cards[1]}, totalBet(0), inHand(true) {}
+    bool hasActed;
+
+    PlayerState(String mac, uint8_t cards[2], uint32_t chips) : playerMAC(mac), holeCards{cards[0], cards[1]}, remainingChips(chips), activeBet(0), inactiveBet(0), hasActed(false) {}
 };
 struct PlayerResult {
     const String playerMAC;
@@ -21,34 +23,48 @@ struct PlayerResult {
     PlayerResult(String mac, double value, int32_t gain) : playerMAC(mac), handValue(value), netGain(gain) {}
 };
 
+struct Pot {
+    const uint32_t size;
+    const std::vector<String> eligiblePlayers; // MAC addresses of players eligible to win this pot
+
+    Pot(uint32_t s, std::vector<String> players) : size(s), eligiblePlayers(players) {}
+};
 class GameState {
 private:
     const uint8_t communityCards[5];
+    const uint32_t smallBlind;
+    const uint32_t bigBlind;
 
-    std::vector<PlayerState> players;
-    uint8_t playerTurn; // Vector index of player who has action
-    uint8_t street; // 0, 1, 2, 3
+    std::vector<PlayerState> playersInHand;
+    std::vector<PlayerState> playersOutOfHand;
+
+    uint8_t playerTurn; // Vector index of player whose turn it is to act
+    uint8_t street; // 0, 1, 2, 3, 4 (game finished)
+
+    uint32_t callAmount; // Amount player must call to stay in hand. 0 if no bet has been made this street.
+    uint32_t lastAnyRaiserIndex; // Vector index of last player to raise, minRaise met or not. -1 if no raises this street.
+    uint32_t minRaiseIncrement; // Size of last bet or raise. Used to calculate minimum raise amount. 0 if no bet has been made this street
+
+    void incTurn(bool folding = false);
+    bool lastAction();
+    void nextStreet();
 public:
-    GameState(const int deck[52], std::vector<String> playerMACs) : communityCards{deck[0], deck[1], deck[2], deck[3], deck[4]} {};
+    GameState(const int deck[52], const uint32_t smallBlind, const uint32_t bigBlind, const std::vector<String> playerMACs) : communityCards{deck[0], deck[1], deck[2], deck[3], deck[4]}, smallBlind(smallBlind), bigBlind(bigBlind) {}
 
-    bool addBet(String playerMAC, uint32_t amount);
-
-    std::optional<uint32_t> getBet(String playerMAC);
+    bool check();
+    bool call();
+    bool betRaise(uint32_t amount);
+    bool fold();
 
     /**
     @returns a vector of the current pots, including side pots if applicable. The first element is the main pot.
     */
-    std::vector<uint32_t> getPotSizes();
+    std::vector<Pot> getPots();
 
     /**
     @returns the amount a player must call to stay in the hand. Nullopt if player not found.
     */
     std::optional<uint32_t> getCallSize(String playerMAC);
-
-    /**
-    @returns true if the game has not yet reached a terminal state, false if the game is over and should be reset.
-    */
-    bool nextState();
 
     std::vector<PlayerResult> getResults();
 };
